c8cf089c1c0b68448e1cc3dc536d13d431dae7fc
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-02-14 10:48:37 +0000

    basic list of contacts

--- templates/civimobile.home.tpl.php
+++ templates/civimobile.home.tpl.php
@@ -5,24 +5,14 @@
 	<div data-role="header">
 		<h1>CiviMobile</h1>
 	</div><!-- /header -->
-
-	<div data-role="content" id="contact-list">
-		<!--
-		//-->
-		<ul data-role="listview">
-		<?php
-		civicrm_initialize();
-		$results=civicrm_api("Contact","get", array ('version' =>'3'));
-		$results=$results['values'];
-		$num = 0;
-		// foreach ($results as $contact)
-		// 		    echo "<li><a href='/contact/".$contact['contact_id']."'>".$contact['display_name']."</a></li>";
-		?>
-		</ul>
-		<div data-role="fieldcontain">
-		    <input type="search" name="search" placeholder="Search for contacts" id="search" value="" />
-		</div>
-		
+	
+	<div data-role="content" id="contact-content"> 
+    <div class="ui-listview-filter ui-bar-c">
+        <input type="search" name="search" placeholder="Search for contacts" id="search" value="" />
+    </div>
+	<div id="contact-list">
+    </div>
+	</div>
 </div><!-- /page -->
 <?php require('civimobile.navbar.php'); ?>
 
@@ -35,7 +25,7 @@ $( function(){
     contactSearch(value);
 	});
 });
-function contactSearch (q){
+function contactSearch(q) {
     $.mobile.showPageLoadingMsg( 'Searching' );
     $().crmAPI ('Contact','get',{'version' :'3', 'sort_name': q, 'return' : 'display_name,phone' }
           ,{ 
@@ -43,18 +33,21 @@ function contactSearch (q){
             success:function (data){
 			console.log(data.values);
               if (data.count == 0) {
-				alert("no results");                            
+				$("#contact-list").empty();
+				$("#contact-list").append("no results found");
               }
               else {
-          alert("some results");
-			// $.each(data, function(index, value) {
-			// 		            $('#months').append('<li>' + value + '</li>');
-			// 		        });
-			//               }
+          //alert("some results");
+			$("#contact-list").empty();
+			$.each(data.values, function(index, value) {
+				$('#contact-list').append("<li class='ui-li ui-li-static ui-body-c'>" + value.display_name + "</li>");
+			});
+			              
+			}
            $.mobile.hidePageLoadingMsg( );
-
-          }
-   });
+          			
+			}
+	});
 }
 // $.each(data.values, function(key, value) {
 //     $('#contacts').append('<li role="option" tabindex="-1" data-ajax="false" data-theme="c" id="event-'+value.contact_id+'" ><a href="#contact/'+value.contact_id+'" data-role="contact-'+value.contact_id+'">'+value.display_name+'</a></li>');
