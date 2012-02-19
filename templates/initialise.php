80c0ed4fe930fcce843f0084068ea2d907c8b3c9
Peter McAndrew <petermcandrew@thirsectordesign.org>
2012-02-19 14:54:57 +0000

    fixing paths

--- templates/initialise.php
+++ templates/initialise.php
@@ -2,7 +2,7 @@
 
 require_once '/var/www/mobile/sites/default/civicrm.settings.php';
 //require_once '/clients/mobile/drupal/sites/all/modules/civicrm/CRM/Core/Config.php';
-require_once '/clients/mobile/drupal/sites/all/modules/civicrm/api/api.php';
+require_once '/var/www/mobile/sites/all/modules/civicrm/api/api.php';
 //CRM_Core_Config::singleton( );
 
 ?>
